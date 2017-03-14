a4bc9a228ce293c5e5a40da665477f3264f9e882
MattMont <matthew.montgomery23@gmail.com>
2017-03-14 15:05:37 -0600

    add migration for table

---
+++ graphProj/database/migrations/2017_03_09_164856_trade.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Trade extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trade', function (Blueprint $table) {
+            $table->string('Year', 50);
+            $table->string('Reporter', 50);
+            $table->string('Partner', 50);
+            $table->string('Commodity',100);
+            $table->biginteger('Import');
+            $table->string('Conternt',50);
+            $table->biginteger('Export');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trade');
+    }
+}
